76de082af7cb81f01a7a165a73c26cc40c444f91
ZayenJS <davidnogueira.dev.pro@gmail.com>
2025-12-23 17:36:16 +0100

    i18n actions

--- lib/widgets/result_card.widget.dart
+++ lib/widgets/result_card.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:rollit/models/dice_action.model.dart';
 
 class ResultCard extends StatelessWidget {
@@ -44,6 +45,7 @@ class ResultCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final localizedActionText = (actionText ?? '').tr();
     final badgeWidgets = <Widget>[];
     for (final constraint in constraints) {
       if (constraint.type == 'duration') {
@@ -154,7 +156,7 @@ class ResultCard extends StatelessWidget {
 
             // --- TEXTE D’ACTION ---
             Text(
-              actionText ?? '',
+              localizedActionText,
               textAlign: TextAlign.center,
               style: const TextStyle(
                 fontSize: 24,
